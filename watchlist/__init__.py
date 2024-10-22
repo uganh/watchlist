fd6be4b937f44e4c2307ac53f8723ac5b3e809e8
uganh16 <uganh16@gmail.com>
2024-10-22 23:06:16 +0800

    Organize application with package

---
+++ watchlist/__init__.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+
+WIN = sys.platform.startswith('win')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///' if WIN else 'sqlite:////') + \
+    os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from watchlist.models import User
+    return User.query.get(int(user_id))
+
+
+@app.context_processor
+def inject_user():
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user)
+
+
+from watchlist import commands, errors, views
---
+++ watchlist/commands.py
@@ -0,0 +1,63 @@
+import click
+
+from watchlist import app, db
+from watchlist.models import Movie, User
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def init(drop):
+    """Initialize the databases."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    name = 'uganh16'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    db.session.add(User(name=name))
+    for movie in movies:
+        db.session.add(Movie(title=movie['title'], year=movie['year']))
+    db.session.commit()
+
+    click.echo('Done.')
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    """Create user."""
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo('Done.')
